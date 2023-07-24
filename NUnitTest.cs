f13e9aa3a743d1b0f204d581513648eb308c91a1
andreea-qa <andreea.draniceanu@gmail.com>
2023-07-24 20:27:08 +0300

    Added one more test & explicit waits

--- NUnitTest.cs
+++ NUnitTest.cs
@@ -1,9 +1,13 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace NUnitDataDriven
 {
+
+    [Parallelizable(ParallelScope.Children)]
     public class NUnitTest
     {
         private static IWebDriver driver;
@@ -31,13 +35,31 @@ namespace NUnitDataDriven
         [TestCase("Components")]
         [TestCase("Cameras")]
         [TestCase("Software")]
-        [Parallelizable(ParallelScope.All)]
         public void OpenCategory(string menuOption)
         {
             driver.Navigate().GoToUrl("https://ecommerce-playground.lambdatest.io/");
             driver.FindElement(By.XPath("//a[normalize-space()='Shop by Category']")).Click();
-            driver.FindElement(By.XPath($"//span[normalize-space()='{menuOption}']")).Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            string xpath = $"//span[normalize-space()='{menuOption}']";
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(xpath)));
+            driver.FindElement(By.XPath(xpath)).Click();
             Assert.That(Equals(driver.Title, menuOption));
+        }
+
+        [Test]
+        [TestCase("andreea", "test")]
+        [TestCase("andreea@getnada.com", "")]
+        [TestCase("andreea@getnada.com", "xxxx")]
+        public void LoginInvalidCredentials(string email, string password)
+        {
+            driver.Navigate().GoToUrl("https://ecommerce-playground.lambdatest.io/index.php?route=account/login");
+            driver.FindElement(By.Name("email")).SendKeys(email);
+            driver.FindElement(By.Id("input-password")).SendKeys(password);
+            driver.FindElement(By.CssSelector("input[value='Login']")).Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            string xpath = "//div[@class='alert alert-danger alert-dismissible']";
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(xpath)));
+            Assert.That(driver.FindElement(By.XPath(xpath)).Text.Contains("No match for E-Mail Address and/or Password."));
         }
 
         [TearDown]
